e893b519a102956fccffa9ebb3958782f220f35b
Ruamberg Vasconcelos Aguiar <ruamberg@users.noreply.github.com>
2017-08-04 15:31:31 -0300

    login

--- app.py
+++ app.py
@@ -8,6 +8,32 @@ from flask.ext.login import LoginManager, UserMixin, login_required, login_user,
 
 app = Flask(__name__)
 
+app.config.update(
+    DEBUG = True,
+    SECRET_KEY = 'secret_xxx'
+)
+
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+# silly user model
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_secret"
+        
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+# create some users with ids 1 to 20       
+users = [User(id) for id in range(1, 21)]
+
 @app.route('/')
 def main():
     return redirect(url_for('login'))

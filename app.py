093d7178ec045440cdab0eed72408128ba19510d
Ruamberg Vasconcelos Aguiar <ruamberg@users.noreply.github.com>
2017-08-04 12:15:36 -0300

    aff

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import render_template
 from flask import redirect
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return redirect("http://webfrios.herokuapp.com/login", code=302)'
+    return redirect("http://webfrios.herokuapp.com/login", code=302)
 
 @app.route('/index')
 def index():

a5e5c1bf78da5a1f2fcdd4c5359850bfed7c7eb6
Ruamberg Vasconcelos Aguiar <ruamberg@users.noreply.github.com>
2017-08-04 15:27:30 -0300

    login lib

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import redirect
 from flask import url_for
 from flask import render_template
 
+from flask import Response, request, session, abort
+from flask.ext.login import LoginManager, UserMixin, login_required, login_user, logout_user
+
 app = Flask(__name__)
 
 @app.route('/')

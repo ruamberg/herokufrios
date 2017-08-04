765833f5eec07460eadb54ed153d51e5feeb569b
Ruamberg Vasconcelos Aguiar <ruamberg@users.noreply.github.com>
2017-08-04 11:24:54 -0300

    add pages

--- app.py
+++ app.py
@@ -4,5 +4,14 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def main():
+    return 'redirect...'
+
+@app.route('/index')
+def index():
     return render_template('index.html')
+    
+@app.route('/login')
+def login():
+    return render_template('login.html')
+    
